9396e192d55ab95b162a3fa014e515eacefe6221
ckx9798 <ckx12@naver.com>
2024-12-31 21:16:12 +0900

    Feat: 글로벌 css 및 메인 폰트 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#6D6AFE",
+        red01: "#FF5B56",
+        black01: "#111322",
+        white01: "#FFFFFF",
+        gray01: "#3E3E4E",
+        gray02: "#9FA6B2",
+        gray03: "#CCD5E3",
+        gray04: "#E7EFFB",
+        gray05: "#F0F6FF",
+        gray06: "#666666",
+      },
+    },
   },
   plugins: [],
-}
+};
