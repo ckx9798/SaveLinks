5c02d5c660eb696b1da7c648ea39c9f52ac1275d
ckx9798 <ckx12@naver.com>
2024-12-31 21:16:26 +0900

    Chore: 라우터 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import "./App.css";
+import "./styles/global.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Links from "./pages/Links";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Todos from "./pages/Todos";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element="Home" />
-        <Route path="/login" element="Login" />
-        <Route path="/login" element="SignUp" />
-        <Route path="/links" element="Links" />
-        <Route path="/todos" element="Todos" />
+        <Route path="/" element={<Home />} />
+        <Route path="/links" element={<Links />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/todos" element={<Todos />} />
       </Routes>
     </BrowserRouter>
   );
