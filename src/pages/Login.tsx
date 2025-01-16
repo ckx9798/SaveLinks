f3f2b69e97aeaf500622815c9a025ba3c66331fb
ckx9798 <ckx12@naver.com>
2025-01-16 23:53:01 +0900

    Feat: 로그인 성공시 links 페이지로 이동

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,10 @@
 import Button from "../components/Button";
 import CommonInput from "../components/CommonInput";
+import { getFolder } from "../api/folder";
+import { postLogin } from "../api/login";
 import schema from "../zod/LoginZod";
 import { useForm } from "react-hook-form";
-import z from "zod";
+import { useNavigate } from "react-router-dom";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 export default function Login() {
@@ -11,17 +13,40 @@ export default function Login() {
     handleSubmit,
     formState: { errors },
   } = useForm({ resolver: zodResolver(schema) });
-  const onsubmit = (data) => console.log(data);
+
+  const navigate = useNavigate();
+  const onsubmit = (data) => {
+    const { email, password } = data;
+    postLogin(email, password)
+      .then((response) => {
+        if (response.status == 200) {
+          navigate("/links");
+        } else {
+          alert("로그인에러");
+        }
+      })
+      .catch((error) => console.error(error));
+  };
+
+  const check1 = () => {
+    getFolder();
+  };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <form className="w-full max-w-[400px] flex flex-col" onSubmit={handleSubmit(onsubmit)}>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div>
+        <img src="/savelinks.svg" width={180} />
+        <p>회원이 아니신가요?</p>
+        <a>회원가입하기</a>
+      </div>
+      <form className="flex w-full max-w-[400px] flex-col" onSubmit={handleSubmit(onsubmit)}>
         <CommonInput register={register} name="email" placeholder="이메일을 입력해주세요" labelName="이메일" />
-        {errors.email && <p>{errors.email.message}</p>}
+        {errors.email && <p className="mb-3 text-red-500">{errors.email.message}</p>}
         <CommonInput register={register} name="password" placeholder="비밀번호를 입력해주세요" labelName="비밀번호" />
-        {errors.password && <p>{errors.password.message}</p>}
+        {errors.password && <p className="mb-3 text-red-500">{errors.password.message}</p>}
         <Button size="response" text="로그인" />
       </form>
+      <button onClick={check1}>asdasd</button>
     </div>
   );
 }
