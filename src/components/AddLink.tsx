94d126fb14005f6d89deab580fa07d7a80bec900
ckx9798 <ckx12@naver.com>
2025-01-21 21:45:52 +0900

    Feat: 새로운 링크 추가 로직 구현

--- src/components/AddLink.tsx
+++ src/components/AddLink.tsx
@@ -1,40 +1,32 @@
 import Button from "./Button";
-import Modal from "./Modal";
-import { postLinks } from "../api/links";
+import SelectFolderModal from "./SelectFolderModal";
 import { useState } from "react";
 
-export default function AddLink() {
-  const [link, setLink] = useState("");
+export default function AddLink({ folderList }) {
+  const [newLink, setNewLink] = useState("");
   const [isModal, setIsModal] = useState(false);
 
   const handleModalOpen = () => {
     setIsModal(true);
   };
 
-  const handleAddLink = (e) => {
-    e.preventDefault();
-    // setIsModal(true);
-    console.log(1);
-    // postLinks();
-  };
   return (
     <>
-      <div
-        className="flex max-h-[120px] w-full max-w-[800px] items-center justify-between rounded-xl border border-primary bg-white px-3 py-2 md:px-5 md:py-2"
-        onSubmit={handleAddLink}
-      >
+      <div className="flex max-h-[120px] w-full max-w-[800px] items-center justify-between rounded-xl border border-primary bg-white px-3 py-2 md:px-5 md:py-2">
         <div className="flex w-full gap-5">
           <img src="/link.svg" />
           <input
             placeholder="링크를 추가해 보세요"
-            className="mr-5 w-full bg-inherit px-2 text-2xl md:py-2"
-            value={link}
-            onChange={(e) => setLink(e.target.value)}
+            className="mr-5 w-full bg-inherit px-2 text-2xl focus:outline-none md:py-2"
+            onChange={(e) => {
+              setNewLink(e.target.value);
+              console.log(newLink);
+            }}
           />
         </div>
         <Button size="xs" text="추가하기" onClick={handleModalOpen} />
       </div>
-      {isModal && <Modal setIsModal={setIsModal} />}
+      {isModal && <SelectFolderModal setIsModal={setIsModal} folderList={folderList} newLink={newLink} />}
     </>
   );
 }
