308a9001e8f304b115a8220f5691d1fe03e54ff7
ckx9798 <ckx12@naver.com>
2025-02-06 19:53:01 +0900

    Feat: 모달에 hover / active 효과 추가

--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,6 +1,9 @@
-export default function Folder({ folderName }) {
+export default function Folder({ folderName, onClick }) {
   return (
-    <div className="rounded-xl border border-primary px-4 py-1 text-2xl hover:bg-primary hover:text-white">
+    <div
+      className="cursor-pointer rounded-md border border-primary px-2 py-1 hover:bg-primary hover:text-white md:rounded-xl md:px-4 md:py-1 md:text-2xl"
+      onClick={onClick}
+    >
       {folderName}
     </div>
   );
