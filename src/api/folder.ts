05bc10652b623ecbf277f8974b15de48528f6735
ckx9798 <ckx12@naver.com>
2025-02-08 03:32:44 +0900

    Feat: 폴더 삭제 및 이름수정 api 추가

--- src/api/folder.ts
+++ src/api/folder.ts
@@ -21,3 +21,25 @@ export const postFolder = async (newFolderName) => {
     throw error;
   }
 };
+
+export const deleteFolder = async (folderId) => {
+  try {
+    const response = await apiClient.delete(`/folders/${folderId}`);
+    return response.data;
+  } catch (error) {
+    console.error("deleteFolder 에러", error.response?.data || error.message);
+    throw error;
+  }
+};
+
+export const changeFolderName = async (folderId, newFolderName) => {
+  try {
+    const response = await apiClient.put(`/folders/${folderId}`, {
+      name: newFolderName,
+    });
+    return response.data;
+  } catch (error) {
+    console.error("changeFolderName 에러", error.response?.data || error.message);
+    throw error;
+  }
+};
