a577476e5215a7b983836e77a409c25bfda41bb7
ckx9798 <ckx12@naver.com>
2025-02-19 19:24:16 +0900

    Feat: 링크 드롭다운 추가

--- src/components/LinkItem.tsx
+++ src/components/LinkItem.tsx
@@ -1,4 +1,7 @@
 import ChangeDate from "../utils/ChangeDate";
+import LinkDeleteDropdownModal from "./Modal/LinkDeleteDropdownModal";
+import LinkEditDropdownModal from "./Modal/LinkEditDropdownModal";
+import LinksDropdown from "./LinkDropdown";
 import { TiStarFullOutline } from "react-icons/ti";
 import { putFavorite } from "../api/links";
 import { useState } from "react";
@@ -33,28 +36,61 @@ export default function LinkItem({ link }) {
     }
   };
 
+  // 드롭다운
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const handleDropdownClick = (e) => {
+    e.preventDefault();
+    setIsDropdownOpen((prev) => !prev);
+  };
+
+  // 드롭다운 모달
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
   return (
-    <a href={nomalizeUrl(link.url)} target="_blank">
-      <div className="relative h-[260px] w-full overflow-hidden rounded-xl shadow-xl md:h-[440px] md:max-w-[400px] lg:max-w-[340px]">
-        {eachLink.favorite ? (
+    <>
+      <a href={nomalizeUrl(link.url)} target="_blank">
+        <div className="relative h-[260px] w-[270px] overflow-hidden rounded-xl shadow-xl hover:scale-110 md:h-[440px] md:w-[400px] lg:w-[340px]">
+          {/* 즐겨찾기 아이콘 */}
           <TiStarFullOutline
-            className="absolute right-1 top-1 text-3xl text-yellow-300"
+            className={`absolute right-1 top-1 text-3xl ${eachLink.favorite ? "text-yellow-300" : "text-slate-400"}`}
             onClick={handleFavoriteClick}
           />
-        ) : (
-          <TiStarFullOutline className="absolute right-1 top-1 text-3xl text-slate-400" onClick={handleFavoriteClick} />
-        )}
 
-        <img src={linkSource} className={linkSourceClass} />
-        <div className="flex flex-col px-4 py-2">
-          <div className="flex justify-between">
-            <p className="my-1 line-clamp-1 font-semibold text-blue-300 md:text-2xl">{eachLink.title}</p>
-            <img src="/kebab.svg" />
+          {/* 이미지 */}
+          <img src={linkSource} className={linkSourceClass} />
+
+          {/* 내용 영역 */}
+          <div className="flex flex-col px-4 py-2">
+            <div className="relative flex justify-between">
+              {/* 제목 */}
+              <p className="my-1 line-clamp-1 font-semibold text-blue-300 md:text-2xl">{eachLink.title}</p>
+
+              {/* 케밥 메뉴 아이콘 */}
+              <img src="/kebab.svg" onClick={handleDropdownClick} className="mx-4 cursor-pointer" />
+
+              {/* 드롭다운 메뉴 */}
+              {isDropdownOpen && (
+                <LinksDropdown
+                  linkId={link.Id}
+                  setIsEditModalOpen={setIsEditModalOpen}
+                  setIsDeleteModalOpen={setIsDeleteModalOpen}
+                />
+              )}
+            </div>
+
+            {/* 설명 */}
+            <h2 className="my-1 line-clamp-2 min-h-[48px] text-xl leading-6 md:my-2 md:min-h-[64px] md:text-2xl md:font-medium">
+              {link.description}
+            </h2>
+
+            {/* 날짜 */}
+            <p className="text-lg md:text-xl">{ChangeDate(link.createdAt)}</p>
           </div>
-          <h2 className="my-1 line-clamp-2 text-xl leading-6 md:my-2 md:text-2xl md:font-medium">{link.description}</h2>
-          <p className="text-lg md:text-xl">{ChangeDate(link.createdAt)}</p>
         </div>
-      </div>
-    </a>
+      </a>
+      {isEditModalOpen && <LinkEditDropdownModal setIsEditModalOpen={setIsEditModalOpen} linkId={link.id} />}
+      {isDeleteModalOpen && <LinkDeleteDropdownModal setIsDeleteModalOpen={setIsDeleteModalOpen} linkId={link.id} />}
+    </>
   );
 }
