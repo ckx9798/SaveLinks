9e6997a014e7e31f9480301dace7d2c8bae5b125
ckx9798 <ckx12@naver.com>
2025-02-22 13:29:40 +0900

    Feat: 로그인 zod 설정

--- src/zod/SingUpZod.ts
+++ src/zod/SingUpZod.ts
@@ -8,7 +8,11 @@ const schema = z.object({
   password: z
     .string()
     .min(8, { message: "비밀번호는 최소 8자 이상이어야 합니다." })
-    .max(100, { message: "비밀번호는 100자를 초과할 수 없습니다." }),
+    .max(20, { message: "비밀번호는 20자를 초과할 수 없습니다." }),
+  name: z
+    .string()
+    .min(1, { message: "이름은 필수 입력 항목입니다." })
+    .max(20, { message: "이름은 20자를 초과할 수 없습니다." }),
 });
 
 export default schema;
