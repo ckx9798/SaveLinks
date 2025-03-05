29cc1f1f68c7c724772d0e8816969d4e69e92986
ckx9798 <ckx12@naver.com>
2025-03-05 18:32:35 +0900

    Docs: 선택 파트 타입 지정

--- src/components/LinksPage/SerchLinkPart.tsx
+++ src/components/LinksPage/SerchLinkPart.tsx
@@ -1,10 +1,13 @@
-export default function SearchLinkPart({ setSearchLink }) {
+import React from "react";
+import { SearchLinkPartProps } from "../../type/link";
+
+export default function SearchLinkPart({ setSearchLink }: SearchLinkPartProps) {
   return (
     <div className="relative flex w-full items-center justify-center">
       <input
-        className="w-full max-w-[1200px] rounded-xl bg-slate-200 px-6 py-3 text-xl/9"
+        className="w-full max-w-[1200px] rounded-xl bg-slate-200 px-6 py-3 text-xl"
         placeholder="🔎 Please search for the link"
-        onChange={(e) => setSearchLink(e.target.value)}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchLink(e.target.value)}
       />
     </div>
   );
