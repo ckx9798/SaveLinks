9d2cfe5a2eabd60354accd5a31c985597433b0a4
ckx9798 <ckx12@naver.com>
2025-04-17 06:22:33 +0900

    Feat: home 페이지 구현 및 이미지 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,38 @@
 import "aos/dist/aos.css";
 
+import AOS from "aos";
 import Button from "../components/Button";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: false,
+    });
+  }, []);
+
   return (
     <>
-      <div className="flex h-[550px] w-screen flex-col gap-4 bg-[url(/home_bg.png)] bg-cover md:h-[800px] md:gap-10 lg:h-screen">
-        <div className="flex w-full items-center justify-between px-2 pt-4 sm:px-6 md:px-14 md:pt-10 lg:px-28">
-          <img src="/savelinks.svg" onClick={() => navigate("links")} />
-          <Button size="xs" text="로그인" onClick={() => navigate("login")}></Button>
+      <div className="flex h-[550px] w-screen flex-col gap-4 bg-[url(/home_bg.png)] bg-cover md:h-[800px] md:gap-10 lg:h-[1200px]">
+        <div className="flex w-full items-center justify-between pr-2 sm:px-6 md:px-8 lg:px-28">
+          <img src="/saveLinks_logo.png" onClick={() => navigate("links")} className="-ml-6 w-[180px] md:w-[300px]" />
+          <Button size="sm" text="로그인" onClick={() => navigate("login")}></Button>
         </div>
 
-        <div className="flex flex-col items-center justify-center text-white">
+        <div className="-mt-4 flex flex-col items-center justify-center text-white md:-mt-10" data-aos="zoom-in-up">
           <div>
-            <span className="text-3xl font-bold lg:text-7xl">세상의 모든정보</span>
-            <span className="text-3xl lg:text-5xl">를</span>
+            <span className="text-3xl font-bold lg:text-5xl">세상의 모든정보</span>
+            <span className="text-2xl lg:text-4xl">를</span>
           </div>
           <div>
-            <span className="text-3xl lg:text-6xl">쉽게 저장하고 관리해보세요</span>
+            <span className="text-2xl lg:text-6xl">쉽게 저장하고 관리해보세요</span>
           </div>
           <button
-            className="mt-8 rounded-full bg-white px-8 py-1 text-xl font-medium text-black md:py-2 md:text-2xl lg:mt-12 lg:text-4xl"
+            className="mt-6 rounded-full bg-white px-8 py-1 text-xl font-medium text-black md:py-2 md:text-2xl lg:mt-12 lg:text-3xl"
             onClick={() => navigate("links")}
           >
             링크 추가하러가기
@@ -31,7 +40,12 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col items-center bg-[#FAFAFA] pt-8 md:flex-row">
+      <div
+        className="flex flex-col items-center bg-[#FAFAFA] pt-8 md:flex-row"
+        data-aos="fade-down"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine"
+      >
         <div className="flex flex-col items-center md:min-w-[300px] xl:min-w-[700px]">
           <span className="text-3xl font-medium lg:text-4xl xl:text-5xl"> 원하는 링크를 저장하세요</span>
           <div className="mt-2 flex flex-col items-center text-gray-400 lg:text-xl xl:mt-8 xl:text-3xl">
@@ -45,7 +59,12 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col items-center bg-[#F5F5F5] pt-8 md:flex-row">
+      <div
+        className="flex flex-col items-center bg-[#F5F5F5] pt-8 md:flex-row"
+        data-aos="fade-left"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine"
+      >
         <div className="order-2 md:order-1">
           <img src="/home_2.png" alt="2번 이미지" className="-mt-5" />
         </div>
@@ -58,7 +77,12 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col items-center bg-[#FAFAFA] pt-8 md:flex-row">
+      <div
+        className="flex flex-col items-center bg-[#FAFAFA] pt-8 md:flex-row"
+        data-aos="fade-right"
+        data-aos-offset="300"
+        data-aos-easing="ease-in-sine"
+      >
         <div className="flex flex-col items-center md:min-w-[300px] xl:min-w-[700px]">
           <span className="text-3xl font-medium lg:text-4xl xl:text-5xl"> 간단한 메모를 작성해보세요</span>
           <div className="mt-2 flex flex-col items-center text-gray-400 lg:text-xl xl:mt-8 xl:text-3xl">
@@ -67,51 +91,9 @@ export default function Home() {
           </div>
         </div>
         <div>
-          <img src="/home_1.png" alt="3번 이미지" className="-mt-5" />
+          <img src="/home_3.png" alt="3번 이미지" className="-mt-5" />
         </div>
       </div>
-      {/* <div className="flex w-full items-center justify-center bg-white py-20">
-        <div className="flex max-w-[1000px] items-center gap-10">
-          <div className="relative flex-1 overflow-visible">
-            <div>
-              <img src="/homeSelectFolder.jpg" width={800} height={300} />
-            </div>
-          </div>
-          <div className="mr-20">
-            <h2 className="text-4xl font-bold text-gray-800">
-              <span>링크를</span>
-              <span className="text-red-400"> 폴더로 관리</span>
-              <span>하세요</span>
-            </h2>
-            <p className="mt-4 text-gray-500">
-              여러 링크를 폴더에 담아서 관리할 수 있습니다.
-              <br />
-              다양한 링크를 원하는 폴더에 담아보세요
-            </p>
-          </div>
-        </div>
-      </div> */}
-      {/* 
-      <div className="flex w-full items-center justify-center bg-white py-20">
-        <div className="flex max-w-[1000px] items-center gap-10">
-          <div className="mr-20">
-            <h2 className="text-4xl font-bold text-gray-800">
-              <span className="text-red-400">간단한 메모</span>
-              <span>를 작성해보세요</span>
-            </h2>
-            <p className="mt-4 text-gray-500">
-              손쉽게 메모를 생성하고
-              <br />
-              수정하고 삭제해보세요
-            </p>
-          </div>
-          <div className="relative flex-1 overflow-visible">
-            <div>
-              <img src="/homeMemo.jpg" />
-            </div>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 }
