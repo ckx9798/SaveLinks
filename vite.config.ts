75026119a9948cce36d0a7480266003946020b07
ckx9798 <ckx12@naver.com>
2025-04-24 04:03:24 +0900

    Feat: 텍스트 압축 - compression 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import compression from "vite-plugin-compression";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
@@ -10,6 +11,13 @@ export default defineConfig({
       filename: "bundle-analysis.html", // 결과 파일 이름
       gzipSize: true, // gzip 크기 표시
       brotliSize: true, // brotli 크기 표시
+    }),
+    compression({
+      algorithm: "gzip", // gzip or brotliCompress
+      ext: ".gz", // 생성될 파일 확장자
+      verbose: true, // 콘솔 로그 표시
+      disable: false, // 압축 활성화
+      threshold: 2024, // 최소 압축 크기 (2KB 이상만 압축)
     }),
   ],
 });
