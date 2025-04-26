a2baae98ddf536f909c710eab51b28967767acbb
ckx9798 <ckx12@naver.com>
2025-04-26 22:57:10 +0900

    Style: 메모 hover 스타일 수정

--- src/components/Memos/MemoItem.tsx
+++ src/components/Memos/MemoItem.tsx
@@ -8,7 +8,7 @@ export default function MemoItem({ memo, setMemos, selectedMemo, setSelectedMemo
   return (
     <>
       <div
-        className="relative mt-2 h-auto w-full overflow-hidden rounded-xl border-2 border-gray01 bg-gray01 shadow-xl hover:scale-105 lg:p-3"
+        className="group relative mt-2 h-auto w-full overflow-hidden rounded-xl border-2 border-gray01 bg-gray01 shadow-md transition-all duration-300 ease-in-out hover:scale-[1.02] hover:border-primary hover:bg-gradient-to-br hover:from-gray01 hover:to-white/20 hover:shadow-2xl lg:p-3"
         onClick={() => {
           setIsEditMemoModalOpen(true);
           setSelectedMemo(memo);
@@ -16,10 +16,10 @@ export default function MemoItem({ memo, setMemos, selectedMemo, setSelectedMemo
       >
         <div className="flex h-full flex-col justify-between px-2 pb-3">
           <p className="line-clamp-1 text-2xl text-primary md:text-3xl">{memo.title}</p>
-          <h2 className="line-clamp-1 min-h-[72px] text-xl leading-6 text-gray04 md:mb-3 md:min-h-[90px] md:text-2xl md:font-medium">
+          <p className="max-h-[120px] min-h-[120px] overflow-y-auto whitespace-pre-line text-xl leading-6 text-gray04 md:mb-3 md:text-2xl md:font-medium xl:min-h-[130px]">
             {memo.description || `No description provided`}
-          </h2>
-          <p className="-mb-2 hidden text-lg text-gray04 md:block md:text-xl">
+          </p>
+          <p className="-mb-2 mt-2 text-right text-lg text-gray04 md:block md:text-xl">
             {new Date(memo.createdAt).toLocaleDateString()}
           </p>
         </div>
