2c1510adb1e5444413d0ad0864f7508dfb7ac0c9
ckx9798 <ckx12@naver.com>
2025-04-30 11:03:10 +0900

    Feat: Home페이지 내부 요소 컴포넌트화

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import "aos/dist/aos.css";
 
 import AOS from "aos";
 import Button from "../components/Button";
+import { HomeSection } from "../components/HomeSection";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -17,10 +18,15 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex h-screen w-screen flex-col gap-4 bg-[url(/home_bg.png)] bg-cover md:h-[800px] md:gap-10 lg:h-[1200px]">
+      <div className="relative flex h-screen w-screen flex-col gap-4 md:h-[800px] md:gap-10 lg:h-[1200px]">
+        <img
+          src="/home_bg.webp"
+          alt="홈 배경 이미지"
+          className="absolute left-0 top-0 -z-10 h-full w-full object-cover"
+        />
         <div className="flex w-full items-center justify-between pr-2 sm:px-6 md:px-8 lg:px-28">
-          <img src="/saveLinks_logo.png" onClick={() => navigate("links")} className="-ml-6 w-[180px] md:w-[300px]" />
-          <Button size="sm" text="로그인" onClick={() => navigate("login")}></Button>
+          <img src="/saveLinks_logo.webp" onClick={() => navigate("/links")} className="-ml-6 w-[180px] md:w-[300px]" />
+          <Button size="sm" text="로그인" onClick={() => navigate("/login")}></Button>
         </div>
 
         <div className="-mt-4 flex flex-col items-center justify-center text-white md:-mt-10" data-aos="zoom-in-up">
@@ -40,60 +46,31 @@ export default function Home() {
         </div>
       </div>
 
-      <div
-        className="flex h-screen flex-col items-center bg-[#FAFAFA] pt-8 md:flex-row"
-        data-aos="fade-down"
-        data-aos-offset="300"
-        data-aos-easing="ease-in-sine"
-      >
-        <div className="flex flex-col items-center md:min-w-[300px] xl:min-w-[700px]">
-          <span className="text-3xl font-medium lg:text-4xl xl:text-5xl"> 원하는 링크를 저장하세요</span>
-          <div className="mt-2 flex flex-col items-center text-gray-400 lg:text-xl xl:mt-8 xl:text-3xl">
-            <span>나중에 읽고 싶은 글, 다시 보고 싶은 영상,</span>
-            <span>사고 싶은 옷, 기억하고 싶은 모든 것을</span>
-            <span>한 공간에 저장하세요.</span>
-          </div>
-        </div>
-        <div>
-          <img src="/home_1.png" alt="1번 이미지" className="-mt-5" />
-        </div>
-      </div>
+      <HomeSection
+        title="원하는 링크를 저장하세요"
+        descriptions={[
+          "나중에 읽고 싶은 글, 다시 보고 싶은 영상,",
+          "사고 싶은 옷, 기억하고 싶은 모든 것을",
+          "한 공간에 저장하세요.",
+        ]}
+        imgSrc="/home_1.webp"
+        aosType="fade-down"
+      />
 
-      <div
-        className="flex h-screen flex-col items-center bg-[#F5F5F5] pt-8 md:flex-row"
-        data-aos="fade-left"
-        data-aos-offset="300"
-        data-aos-easing="ease-in-sine"
-      >
-        <div className="order-2 md:order-1">
-          <img src="/home_2.png" alt="2번 이미지" className="-mt-5" />
-        </div>
-        <div className="order-1 flex flex-col items-center md:order-2 md:min-w-[300px] xl:min-w-[700px]">
-          <span className="text-3xl font-medium lg:text-4xl xl:text-5xl"> 링크를 폴더로 관리하세요</span>
-          <div className="mt-2 flex flex-col items-center text-gray-400 lg:text-xl xl:mt-8 xl:text-3xl">
-            <span>나만의 폴더를 무제한으로 만들고</span>
-            <span>다양하게 활용할 수 있습니다.</span>
-          </div>
-        </div>
-      </div>
+      <HomeSection
+        title="링크를 폴더로 관리하세요"
+        descriptions={["나만의 폴더를 무제한으로 만들고", "다양하게 활용할 수 있습니다."]}
+        imgSrc="/home_2.webp"
+        imgFirst
+        aosType="fade-left"
+      />
 
-      <div
-        className="flex flex-col items-center bg-[#FAFAFA] pt-8 md:flex-row"
-        data-aos="fade-right"
-        data-aos-offset="300"
-        data-aos-easing="ease-in-sine"
-      >
-        <div className="flex flex-col items-center md:min-w-[300px] xl:min-w-[700px]">
-          <span className="text-3xl font-medium lg:text-4xl xl:text-5xl"> 간단한 메모를 작성해보세요</span>
-          <div className="mt-2 flex flex-col items-center text-gray-400 lg:text-xl xl:mt-8 xl:text-3xl">
-            <span>손쉽게 메모를 생성하고</span>
-            <span>수정하고 삭제해보세요</span>
-          </div>
-        </div>
-        <div>
-          <img src="/home_3.png" alt="3번 이미지" className="-mt-5" />
-        </div>
-      </div>
+      <HomeSection
+        title="간단한 메모를 작성해보세요"
+        descriptions={["손쉽게 메모를 생성하고", "수정하고 삭제해보세요"]}
+        imgSrc="/home_3.webp"
+        aosType="fade-right"
+      />
     </>
   );
 }
