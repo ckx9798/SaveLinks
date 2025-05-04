709f40491b5b14bacc992a7db56765f3de1561fc
ckx9798 <ckx12@naver.com>
2025-05-04 12:46:23 +0900

    Feat: shorts 컴포넌트화

---
+++ src/components/Shorts/ShortsItem.tsx
@@ -0,0 +1,27 @@
+import ShortsDropddown from "./ShortsDropdown";
+import { ShortsItemProps } from "../../api/shorts";
+import { useState } from "react";
+
+export default function ShortsItem({ link }: ShortsItemProps) {
+  const [openDropdownId, setOpenDropdownId] = useState<number | null>(null);
+
+  return (
+    <div key={link.id} className="group relative aspect-[2/3] w-full">
+      <iframe
+        src={link.url.startsWith("http") ? link.url : `https://${link.url}`}
+        className="aspect-[2/3] h-auto w-full rounded-xl hover:border-2 hover:border-primary md:min-h-[635px]"
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+        scrolling="no"
+      />
+      <button
+        className="absolute bottom-0 right-0 z-10 hidden rounded-md px-4 py-1 text-white shadow-md group-hover:block group-hover:bg-primary"
+        onClick={() => setOpenDropdownId((prevId) => (prevId === link.id ? null : link.id))}
+      >
+        . . .
+      </button>
+
+      {openDropdownId === link.id && <ShortsDropddown link={link} setOpenDropdownId={setOpenDropdownId} />}
+    </div>
+  );
+}
