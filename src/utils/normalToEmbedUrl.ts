669508542031d140d18743950cf9cffc33ecc8a3
ckx9798 <ckx12@naver.com>
2025-05-08 02:20:49 +0900

    Feat: 단일 url 처리 로직 추가

--- src/utils/normalToEmbedUrl.ts
+++ src/utils/normalToEmbedUrl.ts
@@ -11,3 +11,13 @@ export const normalToEmbedUrl = (urls: string[]): string[] => {
     return url;
   });
 };
+
+export const normalToEmbedUrlSingle = (url: string): string => {
+  if (isYoutube(url)) {
+    return normalizeYoutubeUrl(url);
+  }
+  if (isInstagramReel(url)) {
+    return normalizeInstagramUrl(url);
+  }
+  return url;
+};
