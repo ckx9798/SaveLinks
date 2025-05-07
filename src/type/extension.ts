39b246b74c99883f8df0ccda939689b8467f5268
ckx9798 <ckx12@naver.com>
2025-05-08 02:20:27 +0900

    Docs: 익스텐션 컴포넌트 타입 설정

---
+++ src/type/extension.ts
@@ -0,0 +1,26 @@
+export interface ShortsObjProps {
+  id: string;
+  url: string;
+  embedUrl: string;
+}
+
+export interface ExtensionShortsProps {
+  shortsObj: ShortsObjProps;
+}
+
+export interface ExtensionDropdownProps {
+  shortsObj: ShortsObjProps;
+  setOpenDropdownId: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+export interface ExtensionFullScreenModalProps {
+  shortsUrl: string;
+  setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setOpenDropdownId: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+export interface ExtensionDeleteModalProps {
+  shortsObj: ShortsObjProps;
+  setIsDeleteModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setOpenDropdownId: React.Dispatch<React.SetStateAction<string | null>>;
+}
