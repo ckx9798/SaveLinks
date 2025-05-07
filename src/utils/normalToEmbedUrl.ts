f9d1f62a0dd6c68202ff5d4651eeb7521b3191fd
ckx9798 <ckx12@naver.com>
2025-05-07 21:53:25 +0900

    Feat: url 통일 로직 추가

---
+++ src/utils/normalToEmbedUrl.ts
@@ -0,0 +1,13 @@
+import { isInstagramReel, isYoutube, normalizeInstagramUrl, normalizeYoutubeUrl } from "./urlUtils";
+
+export const normalToEmbedUrl = (urls: string[]): string[] => {
+  return urls.map((url) => {
+    if (isYoutube(url)) {
+      return normalizeYoutubeUrl(url);
+    }
+    if (isInstagramReel(url)) {
+      return normalizeInstagramUrl(url);
+    }
+    return url;
+  });
+};
