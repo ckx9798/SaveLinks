6a856201d250c5dc1cd3d3c74f5a636f9f6b8740
ckx9798 <ckx12@naver.com>
2025-05-09 00:35:26 +0900

    Feat: Extension 페이지 내 컴포넌트화

--- src/pages/Extension.tsx
+++ src/pages/Extension.tsx
@@ -1,32 +1,26 @@
-import AddShorts from "../components/Shorts/AddShorts";
+import { BackgroundImage } from "../extension/BackgroundImage";
+import ExtensionHeader from "../extension/ExtensionHeader";
 import ExtensionShorts from "../extension/ExtensionShorts";
-import Header from "../components/Header";
+import { getMaxWidth } from "../utils/getMaxWidth";
 import { useSavedUrls } from "../utils/useSavedUrls";
 
 export default function Extension() {
   const { savedUrls } = useSavedUrls();
+  const maxWidth = getMaxWidth(savedUrls.length);
 
   return (
     <>
-      <div className="relative flex w-full flex-col items-center justify-center px-6">
-        <img
-          src="/layout_bg.webp"
-          alt="배경 이미지"
-          className="absolute left-0 top-0 -z-10 h-full w-full object-cover"
-          loading="eager"
-          fetchPriority="high"
-          decoding="async"
-        />
-        <Header />
-        <div className="mb-8 flex h-[80px] w-full items-center justify-center md:h-[120px]">
-          <AddShorts />
-        </div>
+      <div className="relative flex w-full flex-col items-center justify-center px-4 md:px-8 xl:px-10">
+        <BackgroundImage />
+        <ExtensionHeader maxWidth={maxWidth} />
       </div>
 
       <div className="flex h-auto min-h-screen justify-center bg-gray04">
-        <div className="mx-auto grid w-full max-w-[1400px] gap-10 px-2 py-6 sm:grid-cols-[repeat(auto-fit,minmax(400px,1fr))] md:px-8 lg:px-6 xl:px-0">
+        <div
+          className={`mx-auto grid w-full gap-10 px-2 pb-40 pt-12 sm:grid-cols-[repeat(auto-fit,minmax(400px,1fr))] ${maxWidth}`}
+        >
           {savedUrls.map((shortsObj) => (
-            <ExtensionShorts shortsObj={shortsObj} />
+            <ExtensionShorts shortsObj={shortsObj} key={shortsObj.id} />
           ))}
         </div>
       </div>
