2bf51f735d660ee6c48d8b0a07fe2968cbdd27ec
ckx9798 <ckx12@naver.com>
2025-05-16 03:24:41 +0900

    Chore: visualizer 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import compression from "vite-plugin-compression";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
   plugins: [
     react(),
-    visualizer({
-      open: true, // 분석 결과 자동으로 브라우저에서 열기
-      filename: "bundle-analysis.html", // 결과 파일 이름
-      gzipSize: true, // gzip 크기 표시
-      brotliSize: true, // brotli 크기 표시
-    }),
     compression({
       algorithm: "gzip", // gzip or brotliCompress
       ext: ".gz", // 생성될 파일 확장자
