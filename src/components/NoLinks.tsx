ad16f1b68c6d622d53ee0702cbc131ba43a41928
ckx9798 <ckx12@naver.com>
2025-05-16 03:26:06 +0900

    Style: 전체페이지 css 수정 및 타입 추가 구현

--- src/components/NoLinks.tsx
+++ src/components/NoLinks.tsx
@@ -1,6 +1,6 @@
 export default function NoLinks() {
   return (
-    <div className="mx-auto mb-56 flex h-screen w-full max-w-[1200px] flex-col items-center">
+    <div className="mx-auto mb-56 flex h-screen w-full max-w-[1400px] flex-col items-center">
       <img src="/noLinks.svg" width={300} className="pl-4 pt-8 md:pt-20" />
       <p className="-mt-4 text-3xl text-gray01">저장된 링크가 없습니다 </p>
     </div>
--- src/type/modal.ts
+++ src/type/modal.ts
@@ -4,12 +4,14 @@ export interface SelectLinkFolderModalProps {
   setIsModal: React.Dispatch<React.SetStateAction<boolean>>;
   folderList: Folder[];
   newLink: string;
+  setNewLink: React.Dispatch<React.SetStateAction<string>>;
 }
 
 export interface UserDropdownProps {
   onClose: () => void;
   onLinkClick: () => void;
   onMemoClick: () => void;
+  onShortsClick: () => void;
   onLogoutClick: () => void;
 }
 
